b8786218527040ed9a0f64c4526e8413fbc5b2f7
Anatoly-LSP-LIZER <tolik.laptev.02@mail.ru>
2022-01-21 13:32:43 +0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginPage from "./pages/LoginPage/LoginPage";
+
+const App = () => {
+
+    return (
+        <div className="App">
+            <LoginPage/>
+        </div>
+    )
+}
+
+export default App;
